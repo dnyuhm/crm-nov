e6b8b0fada9575383f006ec025c65b292af0109e
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-03 14:15:31 +0100

    Commit 2 : import CoreModule

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,14 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { OrdersRoutingModule } from './orders-routing.module';
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    OrdersRoutingModule
-  ]
+  imports: [CommonModule, OrdersRoutingModule],
 })
-export class OrdersModule { }
+export class OrdersModule {}
