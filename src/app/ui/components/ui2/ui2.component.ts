7c1efa508b751f3424c0688967c61805c1b39de4
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-04 10:24:01 +0100

    Commit 7.1 : tp ui2

---
+++ src/app/ui/components/ui2/ui2.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui2',
+  templateUrl: './ui2.component.html',
+  styleUrls: ['./ui2.component.scss'],
+})
+export class Ui2Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
