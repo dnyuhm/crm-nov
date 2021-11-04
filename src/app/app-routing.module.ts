e111f84f008bc444914331d6a15ec0447a5fc7c0
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-04 16:55:25 +0100

    Commit 16 : Routes orders clients  page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
-import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
-import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: PageSignInComponent },
-  { path: 'sign-up', component: PageSignUpComponent },
-  { path: 'reset', component: PageResetPasswordComponent },
-  { path: 'forgot', component: PageForgotPasswordComponent },
   {
     path: 'orders',
     loadChildren: () =>
