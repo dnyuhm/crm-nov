a55b47adb64f5805593be29f1d1620fde3725b9a
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-09 12:15:34 +0100

    Commit 32 : Formulaire & addOrder

---
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { StateOrder } from 'src/app/core/enums/state-order';
+import { Order } from 'src/app/core/models/order';
+
+@Component({
+  selector: 'app-form-order',
+  templateUrl: './form-order.component.html',
+  styleUrls: ['./form-order.component.scss'],
+})
+export class FormOrderComponent implements OnInit {
+  public states: string[] = Object.values(StateOrder);
+  @Input() init!: Order;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/orders/pages/page-add-order/page-add-order.component.ts
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order';
 
 @Component({
   selector: 'app-page-add-order',
   templateUrl: './page-add-order.component.html',
-  styleUrls: ['./page-add-order.component.scss']
+  styleUrls: ['./page-add-order.component.scss'],
 })
 export class PageAddOrderComponent implements OnInit {
+  public item: Order = new Order();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
