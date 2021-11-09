9d57a81379ab5e2bf4521165c220e74ba26e7841
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-09 14:08:43 +0100

    Commit étape 33 : FormBuilder and ReactiveFormsModule

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 
@@ -8,9 +9,21 @@ import { Order } from 'src/app/core/models/order';
   styleUrls: ['./form-order.component.scss'],
 })
 export class FormOrderComponent implements OnInit {
+  public form!: FormGroup;
   public states: string[] = Object.values(StateOrder);
   @Input() init!: Order;
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      tjmHt: [this.init.tjmHt],
+      nbJours: [this.init.nbJours],
+      tva: [this.init.tva],
+      state: [this.init.state],
+      typePresta: [this.init.typePresta],
+      client: [this.init.client],
+      comment: [this.init.comment],
+      id: [this.init.id],
+    });
+  }
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { FormOrderComponent } from './components/form-order/form-order.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
 import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
-import { FormOrderComponent } from './components/form-order/form-order.component';
 
 @NgModule({
   declarations: [
