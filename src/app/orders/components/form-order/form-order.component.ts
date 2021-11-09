24654a01fa7526475e1edb85e20030b54b623642
dylanmeuriot <dylanmeuriot@gmail.com>
2021-11-09 15:31:23 +0100

    Commit étape 35 : Validators and CSS

--- src/app/orders/components/form-order/form-order.component.ts
+++ src/app/orders/components/form-order/form-order.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 
@@ -21,15 +21,18 @@ export class FormOrderComponent implements OnInit {
       nbJours: [this.init.nbJours],
       tva: [this.init.tva],
       state: [this.init.state],
-      typePresta: [this.init.typePresta],
-      client: [this.init.client],
+      typePresta: [this.init.typePresta, Validators.required],
+      client: [
+        this.init.client,
+        [Validators.required, Validators.minLength(2)],
+      ],
       comment: [this.init.comment],
       id: [this.init.id],
     });
   }
 
   public onSubmit(): void {
-    console.log(this.form.value);
+    console.log(this.form);
     this.submitted.emit(this.form.value);
   }
 }
